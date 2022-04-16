33d682961219ad3d86db3a1d89e05cb433fdffc7
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-04-16 10:31:19 +0800

    utk kegunaan syafiq

---
+++ node_restapi_xml_for_georss.js
@@ -0,0 +1,30 @@
+var request = require('request');
+const express = require("express");
+//const xml = require("xml");
+const app = express();
+
+var headers = {
+    'x-api-key': '5ImHVWTLdpx'
+};
+
+var options = {
+    url: 'http://121.121.90.133:8081/poc/v1/coordinate',
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+        app.get("/", (req, res, next) => {
+          let data = body;
+          res.header("Content-Type", "application/xml");
+          res.status(200).send(data);
+        });
+    }
+}
+
+request(options, callback);
+
+app.listen(6789, () => {
+  console.log("Server is running on port 6789");
+});
