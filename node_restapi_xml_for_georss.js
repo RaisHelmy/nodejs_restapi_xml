93bca8b1b8d03832d61a97ac53361658a558d922
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-04-16 14:22:19 +0800

    change to syafiq format of arcgis geo server

--- node_restapi_xml_for_georss.js
+++ node_restapi_xml_for_georss.js
@@ -1,5 +1,6 @@
 var request = require('request');
 const express = require("express");
+const { create } = require('xmlbuilder2');
 const app = express();
 
 var headers = {
@@ -13,9 +14,28 @@ var options = {
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body);
+        const json_body = JSON.parse(body);
+        const root = create({ version: '1.0' })
+            .ele('rss', {'xmlns:georss': 'http://www.georss.org/georss'})
+              .ele('channel')
+                .ele('title').txt('GeoServer Feed').up()
+                .ele('link').txt('ttp://localhost:8081/v1/georss').up()
+                .ele('description').txt('GeoServer Feed').up()
+                .ele('item')
+                  .ele('name').txt(json_body["device_name"]).up()
+                  .ele('lastUpdate').txt(json_body["timestamp"]).up()
+                  .ele('georss:where')
+                    .ele('gml:Point', {'xmlns:gml':'http://www.opengis.net/gml'})
+                    .ele('gml:pos').txt(json_body["longitude"]+' '+json_body["latitude"]).up()
+                    .up()
+                  .up()
+                .up()
+              .up()
+            .up();
+            const xml_syafiq = root.end({ prettyPrint: true });
+        console.log('Updated and served at port: '+xml_syafiq);
         app.get("/", (req, res, next) => {
-          let data = body;
+          let data = xml_syafiq;
           res.header("Content-Type", "application/xml");
           res.status(200).send(data);
         });
